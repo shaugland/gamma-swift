f49b67610e13a469b29a60afd67fb9bf0638cd20
Seth Haugland <shauglan_stu@kent.edu>
2021-11-22 18:04:13 -0500

    include music and initial settings commit

--- setup.py
+++ setup.py
@@ -2,17 +2,24 @@ import dataset
 
 if __name__ == "__main__":
     taskbook_db = dataset.connect('sqlite:///taskbook.db')  
-    task_table = taskbook_db.get_table('task')
+    # task_table = taskbook_db.get_table('task')
+    # task_table.drop()
+    # task_table = taskbook_db.create_table('task')
+    # task_table.insert_many([
+    #     {"time":0.0, "description":"Do something useful", "list":"today", "completed":True, "tag":"tagOne"},
+    #     {"time":0.5, "description":"Do something fantastic", "list":"today", "completed":False, "tag":"tagTwo"},
+    #     {"time":0.3, "description":"Do something remarkable", "list":"tomorrow", "completed":False, "tag":"tagThree"},
+    #     {"time":0.7, "description":"Do something unusual", "list":"tomorrow", "completed":True, "tag":"tagFour"},
+    #     {"time":0.7, "description":"Do a backflip", "list":"someday", "completed":False, "tag":"tagFive"},
+    #     {"time":0.7, "description":"End world hunger", "list":"someday", "completed":True, "tag":"tagSix"}
+    # ]) 
+    task_table = taskbook_db.get_table('settings')
     task_table.drop()
-    task_table = taskbook_db.create_table('task')
+    task_table = taskbook_db.create_table('settings')
     task_table.insert_many([
-        {"time":0.0, "description":"Do something useful", "list":"today", "completed":True, "tag":"tagOne"},
-        {"time":0.5, "description":"Do something fantastic", "list":"today", "completed":False, "tag":"tagTwo"},
-        {"time":0.3, "description":"Do something remarkable", "list":"tomorrow", "completed":False, "tag":"tagThree"},
-        {"time":0.7, "description":"Do something unusual", "list":"tomorrow", "completed":True, "tag":"tagFour"},
-        {"time":0.7, "description":"Do a backflip", "list":"someday", "completed":False, "tag":"tagFive"},
-        {"time":0.7, "description":"End world hunger", "list":"someday", "completed":True, "tag":"tagSix"}
-    ]) 
+        { 'name': 'includeMusic', 'value': True},
+        { 'name': 'includeTags', 'value': True}
+    ])
     #Pats attempt at login
     # user_table = taskbook_db.get_table('user')
     # user_table.drop()
--- views/settings.tpl
+++ views/settings.tpl
@@ -1,7 +1,42 @@
 % include("header.tpl")
 % include("banner.tpl")
 
+<script>
+    function settingsSubmit(event) {
+        var musicInclude = $("#includeMusic").is(":checked") ? 1 : 0;
+
+        settings = {
+            'name': 'includeMusic',
+            'value': musicInclude
+        };
+
+        $.ajax({url:"api/settings", type:"POST", 
+          data:JSON.stringify(settings), 
+          contentType:"application/json; charset=utf-8",
+          success:settingsSaved});
+    }
+
+    function settingsSaved() {
+        alert("your settings have been successfully saved");
+    }
+
+    $(document).ready(function() {
+        $("#submitButton").click(settingsSubmit);
+        
+        % print([element for element in tasks if element['name'] == 'includeMusic'][0])
+        % if [element for element in tasks if element['name'] == 'includeMusic'][0]['value'] == True:
+            $("#includeMusic").prop('checked', true);
+        % else:
+            $("#includeMusic").prop('checked', false);
+        %end
+    });
+</script>
+
 <p>Welcome to the settings page, more features coming soon</p>
 
+<input type="checkbox" id="includeMusic" name="includeMusic" value="music" />
+<label for="includeMusic">Have background music</label>
 
+<button id="submitButton">Click me to Submit!</button>
+<br />
 % include("footer.tpl")
